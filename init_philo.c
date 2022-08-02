0bcf8bf6d1f44b53d4c326ac170df89accc420db
Yassine Wadday <ywadday@e3r5p6.1337.ma>
2022-08-03 00:49:31 +0100

    initialize philos

--- init_philo.c
+++ init_philo.c
@@ -3,20 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   init_philo.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ywadday <ywadday@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 18:43:22 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/27 18:58:35 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/08/02 23:14:56 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void init_philo(t_philo *philo)
+void init_philos(t_philo *philos, int ac, char **av)
 {
-	philo->number_of_philosophers = 0;
-	philo->time_to_die = 0;
-	philo->time_to_eat = 0;
-	philo->time_to_sleep = 0;
-	philo->number_of_times_each_philosopher_must_eat = 0;
+	int size;
+	int i;
+	
+	size = ft_atoi(av[1]);
+	i = -1;
+	while(++i < size)
+	{
+		philos->id = i + 1;
+		philos->number_of_philosophers = ft_atoi(av[1]);
+		philos->time_to_die = ft_atoi(av[2]);
+		philos->time_to_eat = ft_atoi(av[3]);
+		philos->time_to_sleep = ft_atoi(av[4]);
+		if (ac == 6)
+			philos->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+	}
 }
