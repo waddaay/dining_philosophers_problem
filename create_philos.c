65307d4f807f25dec1275b37154d9916a52f3e92
Yassine Wadday <ywadday@e3r5p6.1337.ma>
2022-08-03 00:48:30 +0100

    allocate for philosopers and threads

--- create_philos.c
+++ create_philos.c
@@ -3,27 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   create_philos.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ywadday <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ywadday <ywadday@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 19:06:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/07/27 22:56:10 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/08/02 23:13:49 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_each_philo	*create_philos(int num)
+int	create_philos(pthread_t	*threads, t_philo *philos, int size)
 {
-	int				i;
-	t_each_philo	*philos;
-	
-	i = 0;
-	philos = (t_each_philo *)malloc(sizeof(t_each_philo) * num);
-	while (i < num)
-	{
-		philos[i].id = i + 1;
-		philos[i].fork = 0;
-		philos[i++].thread = (pthread_t *)malloc(sizeof(pthread_t *)); 
-	}
-	return (philos);
+	// printf("%d", size);
+	if (!(threads = (pthread_t *)malloc(sizeof(pthread_t) * size)))
+		return (0);
+	if (!(philos = (t_philo *)malloc(sizeof(t_philo) * size)))
+		return (0);
+	return (1);
 }
