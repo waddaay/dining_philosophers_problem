398be638cc08c916b92222d8265da39e1884d49b
Yassine Wadday <ywadday@e3r5p6.1337.ma>
2022-08-09 20:52:19 +0100

    header

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: ywadday <ywadday@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 18:17:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/08/02 23:02:30 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/08/09 19:06:33 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,23 @@
 # include <stdio.h>
 #include <pthread.h>
 
+typedef struct s_mutex{
+	pthread_mutex_t *mutex;
+}t_mutex;
+
 typedef struct s_philo{
-    int id;
-    int number_of_philosophers;
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int number_of_times_each_philosopher_must_eat;
+	int	id;
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+	t_mutex *mtx;
 }t_philo;
 
-void    init_philos(t_philo *philos, int ac, char **av);
-int     ft_atoi(char *str);
-int     create_philos(pthread_t	*threads, t_philo *philos, int size);
+int init_philos(t_philo *philos, int ac, char **av, t_mutex *mutex);
+int		ft_atoi(char *str);
+int		create_philos(pthread_t	**threads, t_philo **philos, int size);
+int		check_args(int argc, char **argv);
 
 #endif
