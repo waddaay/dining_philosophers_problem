8859ff12c19789b6c3c8afd9101d972f50ff4c1b
Yassine Wadday <ywadday@e3r5p6.1337.ma>
2022-08-09 20:55:10 +0100

    allocate for mutex

--- create_philos.c
+++ create_philos.c
@@ -6,18 +6,20 @@
 /*   By: ywadday <ywadday@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 19:06:17 by ywadday           #+#    #+#             */
-/*   Updated: 2022/08/02 23:13:49 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/08/09 18:39:55 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	create_philos(pthread_t	*threads, t_philo *philos, int size)
+int	create_philos(pthread_t	**threads, t_philo **philos, int size)
 {
-	// printf("%d", size);
-	if (!(threads = (pthread_t *)malloc(sizeof(pthread_t) * size)))
+	int i;
+	
+	if (!(*threads = (pthread_t *)malloc(sizeof(pthread_t) * size)))
 		return (0);
-	if (!(philos = (t_philo *)malloc(sizeof(t_philo) * size)))
+	if (!(*philos = (t_philo *)malloc(sizeof(t_philo) * size)))
 		return (0);
 	return (1);
 }
+
--- init_philo.c
+++ init_philo.c
@@ -6,27 +6,36 @@
 /*   By: ywadday <ywadday@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 18:43:22 by ywadday           #+#    #+#             */
-/*   Updated: 2022/08/02 23:14:56 by ywadday          ###   ########.fr       */
+/*   Updated: 2022/08/09 19:27:17 by ywadday          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void init_philos(t_philo *philos, int ac, char **av)
+int init_philos(t_philo *philos, int ac, char **av, t_mutex *mutex)
 {
 	int size;
 	int i;
 	
+	
 	size = ft_atoi(av[1]);
 	i = -1;
+	mutex->mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * size);
+	if (!mutex)
+		return (0);
 	while(++i < size)
 	{
-		philos->id = i + 1;
-		philos->number_of_philosophers = ft_atoi(av[1]);
-		philos->time_to_die = ft_atoi(av[2]);
-		philos->time_to_eat = ft_atoi(av[3]);
-		philos->time_to_sleep = ft_atoi(av[4]);
+		philos[i].id = i;
+		philos[i].number_of_philosophers = ft_atoi(av[1]);
+		philos[i].time_to_die = ft_atoi(av[2]);
+		philos[i].time_to_eat = ft_atoi(av[3]);
+		philos[i].time_to_sleep = ft_atoi(av[4]);
+		pthread_mutex_init(&mutex->mutex[i], NULL);
+		philos[i].mtx = mutex;
 		if (ac == 6)
-			philos->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+			philos[i].number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+		else 
+			philos[i].number_of_times_each_philosopher_must_eat = 0;
 	}
+	return (1);
 }
